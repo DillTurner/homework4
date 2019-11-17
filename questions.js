24fb61c7f8416a2264127d6554f3fe3b39e3a5a1
DillTurner <54756447+DillTurner@users.noreply.github.com>
2019-11-16 23:16:13 -0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,25 @@
+var myQuestions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+        title: "What color is the sky?",
+        choices: ["blue", "white", "purple", "red"],
+        answer: "white"
+
+    }
+    {
+        title: "who is president of the USA?",
+        choices: ["Trump", "Franklin", "Johnson", "Kennedy"],
+        answer: "Trump"
+
+    }
+  ];
+  
