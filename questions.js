ebcee5a100d4db7686c945c85ac2287fe25896c0
DillTurner <54756447+DillTurner@users.noreply.github.com>
2020-04-17 15:46:41 -0700

    update

--- questions.js
+++ questions.js
@@ -1,25 +1,78 @@
-var myQuestions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-        title: "What color is the sky?",
-        choices: ["blue", "white", "purple", "red"],
-        answer: "white"
-
-    }
-    {
-        title: "who is president of the USA?",
-        choices: ["Trump", "Franklin", "Johnson", "Kennedy"],
-        answer: "Trump"
-
-    }
-  ];
+window.onload= function(){
+  var display = document.getElementById("quiz");
+  var titleQ = document.getElementById("running-questions");
+  var choiceQ = document.getElementById("running-choices");
+  var timer = document.getElementById("time");
+  var start = document.getElementById("start-quiz");   
   
+  start.addEventListener("click", function(){
+    var count = 0;
+    var timeLeft= 75;
+
+    setInterval(() => {
+      count++
+      timer.textContent= "Time: " + (timeLeft - count);
+      
+      
+    }, 1000);
+
+    var choiceUl = document.createElement("ul");
+    display.replaceChild(choiceUl,choiceQ);
+    var questions = 
+      {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+      }
+      console.log("started game");
+      start.classList.add("invisible");
+      titleQ.textContent = questions.title;
+
+      for(i=0; i<questions.choices.length; i++){
+
+        var button = document.createElement("button");
+        var choiceLi = questions.choices[i];
+        
+        button.setAttribute("data-index", i);
+        button.classList.add("btn","visible","btn-primary","m-3");
+    
+        button.textContent = choiceLi;
+        choiceUl.appendChild(button)
+        
+      }
+      
+      
+    });
+        display.addEventListener("click",function(event,){
+          element = event.target;
+          
+          
+          if(element.matches("button") === true){
+            var index = element.getAttribute("data-index");
+            if(index== 2){
+              var correctAn = document.createElement("p");
+              correctAn.textContent = "You're Right!!";
+              display.appendChild(correctAn);
+              console.log("correct");
+
+            }
+            else if(element.matches("#start-quiz")){
+              
+            }
+            else{
+              var incorrectAn = document.createElement("p");
+              incorrectAn.textContent= "You're wrong!!!";
+              display.appendChild(incorrectAn);
+              setInterval()-15;
+              console.log("incorrect");
+              
+            }
+            
+          }
+         
+        })
+    
+
+    
+    
+  };
